c22836bdd24f5a8e09a408b9da5d62def1dc145b
Q1uLe <renatik.lolosharik@gmail.com>
2024-09-15 01:45:10 +0300

    Some work
    Added part of Point and Circle, created Tester

--- src/ru/mirea/prac2/Point/Circle.java
+++ src/ru/mirea/prac2/Point/Circle.java
@@ -1,4 +1,46 @@
 package ru.mirea.prac2.Point;
 
 public class Circle {
+    Point center;
+    double radius;
+    String color;
+
+    public Circle(Point center, double radius, String color) {
+        this.center = center;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public Point getCenter() {
+        return center;
+    }
+
+    public void setCenter(Point center) {
+        this.center = center;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "center=" + center +
+                ", radius=" + radius +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
