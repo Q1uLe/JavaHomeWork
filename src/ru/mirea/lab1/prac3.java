6896aafc3e3001a279e8e943d5239f642222be49
Q1uLe <renatik.lolosharik@gmail.com>
2024-09-14 23:24:42 +0300

    Fourth commit

--- src/ru/mirea/lab1/prac3.java
+++ src/ru/mirea/lab1/prac3.java
@@ -1,4 +1,14 @@
 package ru.mirea.lab1;
 
 public class prac3{
+    public static void main(String[] args){
+        if (args.length == 0){
+            System.out.println("Нечего выводить");
+        }
+        else{
+            for(int i = 0; i < args.length; i++){
+                System.out.println("Аргумент " + i + " - " + args[i]);
+            }
+        }
+    }
 }
