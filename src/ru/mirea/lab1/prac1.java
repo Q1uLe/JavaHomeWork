535fd8ed10aec9d5c020606fdf79a7799b073097
Q1uLe <renatik.lolosharik@gmail.com>
2024-09-14 22:48:11 +0300

    Second commit

--- src/ru/mirea/lab1/prac1.java
+++ src/ru/mirea/lab1/prac1.java
@@ -1,10 +1,11 @@
 package ru.mirea.lab1;
 import java.util.Scanner;
 
-public class prac1git  {
+public class prac1  {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int i=0, num, flag=0;
+        int i=0, num, flag=0, sum=0;
+        float mid;
         int[] nums = new int[10];
         while (i!=10){
             if (sc.hasNextInt()) {
@@ -19,5 +20,11 @@ public class prac1git  {
         for (i = 0; i < 10; i++) {
             System.out.println(nums[i]);
         }
+        for (i = 0; i < 10; i++) {
+            sum += nums[i];
+        }
+        mid = (float)sum / 10;
+        System.out.println("Sum is: " + sum);
+        System.out.println("Mid is: " + mid);
     }
 }
