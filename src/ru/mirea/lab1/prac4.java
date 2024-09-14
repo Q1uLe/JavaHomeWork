d9dea349c3d3b4af360773a15dafbaf632641e23
Q1uLe <renatik.lolosharik@gmail.com>
2024-09-14 23:34:36 +0300

    Fifth commit

---
+++ src/ru/mirea/lab1/prac4.java
@@ -0,0 +1,13 @@
+package ru.mirea.lab1;
+
+public class prac4 {
+    public static void main(String[] args){
+        int i = 1;
+        float out = 0;
+        for (;i < 11;i++){
+            out = out + (float)1 / i;
+            System.out.printf("%.3f\n", out);
+
+        }
+    }
+}
