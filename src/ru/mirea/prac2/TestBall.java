b38b8f679ef4093426f60885c54a2cd87c57ada1
Q1uLe <renatik.lolosharik@gmail.com>
2024-09-15 01:24:46 +0300

    Solved 2task
    Solved second task in second practice

--- src/ru/mirea/prac2/TestBall.java
+++ src/ru/mirea/prac2/TestBall.java
@@ -1,4 +1,18 @@
 package ru.mirea.prac2;
 
 public class TestBall {
+    public static void main(String[] args) {
+        Ball testBall = new Ball(0, 0);
+        System.out.println(testBall + "\n");
+
+        System.out.println("Getted X is " + testBall.getX() + "\n");
+
+        testBall.setXY(2, 13);
+        System.out.println("New XY is: (" + testBall.getX() + ", " + testBall.getY() + ")");
+
+        testBall.move(-1.5, -0.14);
+        System.out.println("Ball moved to: (" + testBall.getX() + ", " + testBall.getY() + ")");
+
+        System.out.println(testBall);
+    }
 }
