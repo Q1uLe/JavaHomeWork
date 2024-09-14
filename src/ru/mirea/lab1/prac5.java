e35811cd5ae91a5e74093f8e8e1b445d49ff39cd
Q1uLe <renatik.lolosharik@gmail.com>
2024-09-15 00:28:05 +0300

    Add prac5
    Solve 7th task of first lab

---
+++ src/ru/mirea/lab1/prac5.java
@@ -0,0 +1,23 @@
+package ru.mirea.lab1;
+
+public class prac5 {
+    public static int factorial(int x){
+        if (x < 0){
+            System.out.println("Факториал для отрицательных чисел не определён");
+            return -1;
+        }
+        if (x == 0 || x == 1){
+            return 1;
+        }
+        int i = 1, result = 1;
+        for(;i <= x; i++){
+            result *= i;
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        for (String arg : args) {
+            System.out.println(factorial(Integer.decode(arg)));
+        }
+    }
+}
