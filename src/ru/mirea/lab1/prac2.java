a60b4a8d1ecf460b1cc52a30b6fa103e49258acc
Q1uLe <renatik.lolosharik@gmail.com>
2024-09-14 23:13:02 +0300

    Third commit

--- src/ru/mirea/lab1/prac2.java
+++ src/ru/mirea/lab1/prac2.java
@@ -1,4 +1,43 @@
 package ru.mirea.lab1;
+import java.util.Scanner;
 
 public class prac2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num_count = 0;
+        System.out.println("Введите целое число - количество элементов в массиве");
+        while (num_count == 0){
+            if (sc.hasNextInt()){
+                num_count = sc.nextInt();
+            }
+            else{
+                System.out.println("Вы ввели не целое число, попробуйте еще раз");
+                sc.next();
+            }
+        }
+        int[] nums = new int[num_count];
+        int i = 0;
+        int max= 0, min = 0;
+        System.out.println("Последовательно введтие " + num_count + " чисел");
+        while (i!=num_count){
+            if (sc.hasNextInt()){
+                nums[i] = sc.nextInt();
+                if (i == 0){
+                    max = min = nums[i];
+                }
+                if (nums[i] > max){
+                    max = nums[i];
+                }
+                if (nums[i] < min){
+                    min = nums[i];
+                }
+                i++;
+            }
+            else{
+                System.out.println("Вы ввели не целое число, попробуйте еще раз");
+                sc.next();
+            }
+        }
+        System.out.println("Max is: " + max + "\n" + "Min is:" + min);
+    }
 }
