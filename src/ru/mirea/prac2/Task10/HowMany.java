222ed3306c65393b3e3b2e5e669bd9ccd98a97a0
Q1uLe <renatik.lolosharik@gmail.com>
2024-09-23 01:39:12 +0300

    Some lil improvments

--- src/ru/mirea/prac2/Task10/HowMany.java
+++ src/ru/mirea/prac2/Task10/HowMany.java
@@ -3,8 +3,7 @@ package ru.mirea.prac2.Task10;
 public class HowMany {
     public static void main(String[] args) {
         int counter = 0;
-        for (String arg: args){
-//            System.out.println(arg);
+        for (String _ : args){
             counter++;
         }
         System.out.println(counter);
