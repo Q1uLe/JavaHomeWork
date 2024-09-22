65675fd4892c307b08f19f23bc59a61ac48fd0a9
Q1uLe <renatik.lolosharik@gmail.com>
2024-09-23 01:35:42 +0300

    Solved task 10

--- src/ru/mirea/prac2/Task10/HowMany.java
+++ src/ru/mirea/prac2/Task10/HowMany.java
@@ -4,6 +4,7 @@ public class HowMany {
     public static void main(String[] args) {
         int counter = 0;
         for (String arg: args){
+//            System.out.println(arg);
             counter++;
         }
         System.out.println(counter);
