3d432ed06a0a0f712b785f50a1ee180a23422a25
Q1uLe <renatik.lolosharik@gmail.com>
2024-10-03 16:09:55 +0300

    Solved prac3.Task3

---
+++ src/ru/mirea/prac3/Task3/task3.java
@@ -0,0 +1,27 @@
+package ru.mirea.prac3.Task3;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class task3 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] ints = {
+                rand.nextInt(90) + 10,
+                rand.nextInt(90) + 10,
+                rand.nextInt(90) + 10,
+                rand.nextInt(90) + 10,
+        };
+        System.out.println(Arrays.toString(ints));
+        boolean flag = Boolean.TRUE;
+        for(int i = 0; i < 3; i++) {
+            flag = flag && (ints[i] < ints[i + 1]);
+        }
+        if (flag){
+            System.out.println("Последовательность строго возрастает");
+        }
+        else {
+            System.out.println("Последовательность не является строго возрастающей");
+        }
+    }
+}
